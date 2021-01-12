77dea432927f5b83dc00b36d049cd560551ea1d2
Travis Hoppe <travis.hoppe@gmail.com>
2021-01-11 19:14:06 -0500

    Add preprocessing step

--- P0_encode_images.py
+++ P0_encode_images.py
@@ -1,7 +1,59 @@
 from dspipe import Pipe
 from src import clip
+from PIL import Image
 import torch
+import numpy as np
+import pandas as pd
+
+"""
+Preprocessing step:
+Compute latents for all images in "data/source_images".
+Save data to datasets "data/img_latents.npy", "data/img_keys.csv"
+"""
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, transform = clip.load("ViT-B/32", device=device)
 
+f_img_latents = "data/img_latents.npy"
+f_img_keys = "data/img_keys.csv"
+
+
+def compute(f0, f1):
+
+    try:
+        img = Image.open(f0)
+        image = transform(img).unsqueeze(0).to(device)
+    except Exception as EX:
+        print(f"Failed opening {f0} {EX}")
+        return False
+
+    with torch.no_grad():
+        latents = model.encode_image(image)
+
+    # Pull the latents off the GPU
+    latents = latents.detach().cpu().numpy().ravel().astype(float)
+
+    np.save(f1, latents)
+
+
+Pipe(
+    "data/source_images/",
+    "data/image_latents",
+    input_suffix=".jpg",
+    output_suffix=".npy",
+    shuffle=True,
+)(compute, 1)
+
+
+def read(f0):
+    v = np.load(f0).astype(np.float64)
+    return f0.name.split(".npy")[0], v
+
+
+data = Pipe("data/image_latents/")(read, -1)
+keys, V = zip(*data)
+
+df = pd.DataFrame(data=keys, columns=["unsplashID"])
+df.set_index("unsplashID").to_csv(f_img_keys)
+
+np.save(f_img_latents, V)
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Alph, The Sacred River
 _AI poetic imagery using [CLIP](https://github.com/openai/CLIP) and [Unsplash](https://unsplash.com/)_
 
-Work in progress...
+## Preprocessing steps for new images
+
++ Download a bunch of images from Unsplash ([start here!](https://github.com/unsplash/datasets))
++ Save each image as a jpg in `data/source_images/`
++ Run `python P0_encode_images.py` to compute the image latents
+
